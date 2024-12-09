139da141fb52820138aed2a4165dc7d156de65d4
Asiman Gadirov <asiman.gadirov@IT-MAC12.local>
2024-12-09 15:02:17 +0400

    feat(data-structures/array/findArrayDuplicates) : completed array finding duplicates as ascending result

--- data-structures/array/findArrayDuplicates.js
+++ data-structures/array/findArrayDuplicates.js
@@ -3,29 +3,42 @@
 */
 
 function findSecondLargestElementOfArray(arr) {
-    const tempObject = {}
-    const sortedArray = []
+    const tempObject = {};
+    const sortedArray = [];
     for (let i = 0; i < arr.length; i++) {
         let keyInObject = tempObject[arr[i]]
 
         if (keyInObject) {
             tempObject[arr[i]] = ++keyInObject
             if (tempObject[arr[i]] === 2) {
-                sortedArray.push(arr[i])
+                sortedArray.push(arr[i]);
             }
         } else {
             tempObject[arr[i]] = 1
         }
     }
-    let temp = sortedArray[0]
-    for (let i = 0; i < sortedArray; i++) {
-        if (temp > sortedArray[i]) {
-            let subTemp = temp
-            temp = sortedArray[i]
-        }
-        temp = sortedArray[i]
-    }
-    return sortedArray
-}
 
-console.log(findSecondLargestElementOfArray([3, 1, 0, 4, 2, 3, 1]))
+    for (let i = 0; i < sortedArray.length; i++) {
+
+        if (sortedArray.length === 1) {
+            return sortedArray;
+        }
+        let temp = sortedArray[i];
+        let j = i - 1;
+
+        while( j >= 0 && sortedArray[j] > temp) {
+              sortedArray[j+1] = sortedArray[j];
+              j--;
+        }
+        sortedArray[j+1] = temp;
+    }
+
+    /*
+        additional method for sorting:
+        !! return sortedArray.sort((a,b)=>a-b)
+    */
+
+    return sortedArray
+};
+
+console.log(findSecondLargestElementOfArray([1,2,3,2,0,0,4]))
